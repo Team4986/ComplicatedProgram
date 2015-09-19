c6f275a4749e16cfa4171c027933866ea0924850
Bryce <bsoghigian@vermilioncountry>
2015-09-19 13:18:08 -0500

    Create Claw.java

---
+++ Claw.java
@@ -0,0 +1,8 @@
+package edu.wpi.first.SmartBot.subsystems;
+
+
+import edu.wpi.first.SmartBot.RobotMap;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.commands.subsystem;
+
+public class claw extends subsystem{
